2d0e1c80f75ce8397f467a4f25e173c3e00f51d0
isgandar.hasilov <isgandar.hasilov@yahoo.com>
2021-03-15 12:51:45 +0400

    JavaScripte aid verilmis tgapsirigi yazmaga calisdim

---
+++ Tasks/week05day05/script.js
@@ -0,0 +1,9 @@
+function toplama() {
+    let a = prompt("ilk deyeri daxil edin:");
+    let b = prompt("ikinci deyeri daxil edin:");
+    let c = "ilk deyer " + " +" + " ikinci deyer" + "=" + (Number(a) + Number(b));
+
+    document.write(c);
+}
+
+toplama();
