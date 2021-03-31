8b4cca2f86eeffac583ada93d0a2d4d8ee440411
isgandar.hasilov <isgandar.hasilov@yahoo.com>
2021-03-31 10:07:52 +0400

    add some js code

---
+++ Tasks/week08day02/script.js
@@ -0,0 +1,72 @@
+
+	const colors = ['red', 'green', 'blue'];
+	let index = 0;
+	const a = document.querySelector('.container');
+	a.addEventListener('click', e=>{
+		document.querySelector('body').style.backgroundColor = colors[index++];
+		if (index === colors.length) {
+			index = 0;
+		}
+	})
+
+
+
+
+
+    element.addEventListener('click', e=>{
+        // type a code block
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let sliderPhoto = document.querySelector(".slider-photo img");
+// let sliderText = document.querySelector(".slider-text");
+// let leftButton = document.querySelector(".left-button");
+// let rightButton = document.querySelector(".right-button");
+
+// let slideData = [
+//     {
+
+//     }
+// ]
+
+// leftButton.addEventListener('click', function () {
+//     sliderPhoto.setAttribute("src", "1.jpg")
+//     sliderText.innerHTML = "salam dunya!"
+// })
+
+// rightButton.addEventListener('click', function () {
+//     sliderPhoto.setAttribute("src", "3.jpg")
+//     sliderText.innerHTML = "hello world!"
+// })
+
+
+
+
+
