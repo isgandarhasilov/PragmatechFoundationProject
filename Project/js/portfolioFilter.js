2468cfe50cd75561588253c7b29db7aa43dab18b
isgandar.hasilov <isgandar.hasilov@yahoo.com>
2021-04-13 20:14:51 +0400

    ad  js code and database folders

---
+++ Project/js/portfolioFilter.js
@@ -0,0 +1,28 @@
+// console.log(Hello World!)
+// btns here
+let allBtn = document.querySelector(".all");
+let mediaBtn = document.querySelector(".media");
+let illustrationBtn = document.querySelector(".illustration");
+let videoBtn = document.querySelector(".video");
+// ----------------------
+let myMedia = document.querySelectorAll(".myMedia");
+let myIllustration = document.querySelectorAll(".myIllustration");
+let myVideo = document.querySelectorAll(".myVideo");
+// ----------------------
+
+allBtn.addEventListener("click", function(){
+	console.log('allBtn clicked');
+})
+mediaBtn.addEventListener("click", function(){
+	console.log('mediaBtn clicked');
+})
+illustrationBtn.addEventListener("click", function(){
+	console.log('illustrationBtn clicked');
+})
+videoBtn.addEventListener("click", function(){
+	console.log('videoBtn clicked');
+})
+
+// myFunctions here
+
+
