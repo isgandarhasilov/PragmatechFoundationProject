a9d10dbd069b74a057e823837acc201f222c59ad
isgandar.hasilov <isgandar.hasilov@yahoo.com>
2021-04-20 11:35:00 +0400

    add python task

---
+++ Tasks/week10-day01/run.py
@@ -0,0 +1,30 @@
+cumle = "Javascript ne vaxtdan dil olub gedin duz emelli dil oyren!!!"
+print(len(cumle))
+
+# print(cumle())
+
+
+# for herf in cumle:
+#     print(herf)
+# print(len(cumle))
+
+
+
+
+# print(type(cumle))
+
+# a = 5
+# sinif = ["Isgender", "Ulker", 5]
+# print(type(sinif))
+
+# 1. string metodlari
+# 2. 
+
+
+
+
+
+# def isgender(a, b=5):
+#     print(a+b)
+
+# isgender(5, 16)
