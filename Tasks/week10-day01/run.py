f15d3af7e6136f26c55638b50a70d8a57976d780
isgandar.hasilov <isgandar.hasilov@yahoo.com>
2021-04-21 17:16:21 +0400

    add some python code

--- Tasks/week10-day01/run.py
+++ Tasks/week10-day01/run.py
@@ -1,5 +1,5 @@
-cumle = "Javascript ne vaxtdan dil olub gedin duz emelli dil oyren!!!"
-print(len(cumle))
+# cumle = "Javascript ne vaxtdan dil olub gedin duz emelli dil oyren!!!"
+# print(len(cumle))
 
 # print(cumle())
 
@@ -28,3 +28,19 @@ print(len(cumle))
 #     print(a+b)
 
 # isgender(5, 16)
+
+
+
+
+# a=list(input().split())
+# c=a[0]
+# d=a[1]
+# print(int(c)+int(d))
+#  0  1  2  3   4    5    6   
+a=[3, 6, 9, 12, 13, -67, -70]
+for i in a:
+    for j in a:
+        if i>j:
+            eded=j
+            i=eded
+print(eded)
